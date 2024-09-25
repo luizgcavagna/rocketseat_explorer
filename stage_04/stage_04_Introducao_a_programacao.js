0bdd17771b82aef1e67369f5c8cab7d630fe2222
Luiz Gustavo Cavagna <94694693+luizgcavagna@users.noreply.github.com>
2024-09-25 15:34:02 -0300

    adicionado joguinho de adivinhacao

--- stage_04/stage_04_Introducao_a_programacao.js
+++ stage_04/stage_04_Introducao_a_programacao.js
@@ -96,4 +96,33 @@ else
   alert(lista);
 
 */
+/*
+  ** Jogo da advinhação **
+
+  Apresente a mensagem ao usuário:
+  "Advinhe o número que estou pensando? Está entre 0 e 10"
+
+  Crie um lógica para gerar um número aleatório 
+  e verificar se o número digitado é o mesmo que o aleatório gerado pelo sistema.
+
+  Enquanto o usuário não adivinhar o número, mostrar a mensagem:
+  "Erro, tente novamente:"
+
+  Caso o usuário acerte o número, apresentar a mensagem:
+  "Parabéns! Você advinhou o número em x tentativas"
+
+  Substitua o "x" da mensagem, pelo número de tentativas
+
+  let number = Number(prompt('Advinhe o número que estou pensando? Está entre 0 e 10'));
+  let numberOfChances = Math.round(Math.random() * 10);
+  let attempts = 1;
+
+  while(number != numberOfChances){
+    number = Number(prompt('Erro, tente novamente: '));
+    attempts++;
+  }
+
+  console.log(`Parabéns! Você advinhou o número em ${attempts} tentativas.`)
+
+*/
 
