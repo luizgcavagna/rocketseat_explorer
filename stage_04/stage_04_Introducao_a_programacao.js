07c291726345ffe5795a8393fd4530c31b37b8f3
Luiz Gustavo Cavagna <94694693+luizgcavagna@users.noreply.github.com>
2024-09-25 19:14:07 -0300

    Update stage_04_Introducao_a_programacao.js

--- stage_04/stage_04_Introducao_a_programacao.js
+++ stage_04/stage_04_Introducao_a_programacao.js
@@ -125,4 +125,59 @@ else
   console.log(`Parabéns! Você advinhou o número em ${attempts} tentativas.`)
 
 */
+/*
+
+  Faça um programa que tenha um menu e apresente a seguinte mensagem:
+  
+  Olá usuário! Digite o número da opção desejada
+
+    1. Cadastrar um item na lista
+    2. Mostrar itens cadastrados
+    3. Sair do programa
+  
+  --- 
+  O programa deverá capturar o número digitado pelo usuário e mostrar o seguintes cenários:
+
+  Caso o usuário digite 1, ele poderá cadastrar um item em uma lista
+  Caso o usuário digite 2, ele poderá ver os itens cadastrados
+    Se não houver nenhum item cadastrado, mostrar a mensagem: 
+      "Não existem itens cadastrados"
+  Caso o usuário digite 3, a aplicação deverá ser encerrada.
+
+console.log(`
+  Olá usuário! Digite o número da opção desejada
+
+    1. Cadastrar um item na lista
+    2. Mostrar itens cadastrados
+    3. Sair do programa`
+);
+
+let option;
+let items = [];
+
+while(option != 3) {
+  option = Number(prompt('Selecione uma opcao: '));
+
+  switch(option){
+    case 1:
+      console.log(option);
+      let item = prompt('Cadastrar item: ');
+      if(item.length != 0){
+        items.push(item);
+        console.log(`${item} cadastrado com sucesso!`);
+      }
+      break;
+    case 2:
+      if(items.length != 0){
+        for(let item of items)
+          console.log(`${item}`);
+      } else 
+        console.log('Não existem itens cadastrados')
+      break;
+    case 3:
+      console.log('Isso e tudo pessoal');
+  }
+}
+
+*/
 
