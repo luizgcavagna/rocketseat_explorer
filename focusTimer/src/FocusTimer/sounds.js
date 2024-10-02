bdc11a9f89bee63ac676c902eb65b64ba77ed1ea
Luiz Gustavo Cavagna <94694693+luizgcavagna@users.noreply.github.com>
2024-10-02 15:22:35 -0300

    finalizacao do projeto

---
+++ focusTimer/src/FocusTimer/sounds.js
@@ -0,0 +1,7 @@
+export const buttonPressAudio = new Audio('./assets/sounds/button-press.wav');
+
+export const kitchenTimer = new Audio('./assets/sounds/kitchen-timer.mp3');
+
+export const bgAudio = new Audio('./assets/sounds/bg-audio.mp3');
+
+bgAudio.loop = true;
