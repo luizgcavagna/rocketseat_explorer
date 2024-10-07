5914042cafccf306e9ed135000b63274268b1ee7
Luiz Gustavo Cavagna <94694693+luizgcavagna@users.noreply.github.com>
2024-10-07 16:04:14 -0300

    projeto finalizado

--- github-favorites/js/Favorites.js
+++ github-favorites/js/Favorites.js
@@ -1,3 +1,5 @@
+import { GithubUser } from "./GithubUser.js";
+
 export class Favorites{
   constructor(root) {
     this.root = document.querySelector(root);
@@ -5,24 +7,42 @@ export class Favorites{
   }
 
   load() {
-    this.entries = [{
-        login: 'maykbrito',
-        name: 'Mayk Brito',
-        public_repos: '46',
-        followers: '12000'
-      },
-      {
-        login: 'diego3g',
-        name: 'Diego Fernandes',
-        public_repos: '76',
-        followers: '32709'
-      }];
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites')) || [];
+  }
+
+  save() {
+    localStorage.setItem('@github-favorites', JSON.stringify(this.entries));
+  }
+
+  async add(username) {
+    try {
+
+      const userExists = this.entries.find(entry => entry.login === username);
+
+      if(userExists) {
+        throw new Error('Usuário já cadastrado!');
+      }
+
+      const user = await GithubUser.search(username);
+
+      if (user.login === undefined) 
+        throw new Error('Usuário não encontrado!');
+
+      this.entries = [user, ...this.entries];
+      this.update();  
+      this.save();
+
+    } catch (error) {
+      alert(error.message);
+    }
   }
 
   delete(user) {
     const filteredEntries = this.entries.filter( entry => user.login != entry.login);
 
-    console.log(filteredEntries);
+    this.entries = filteredEntries
+    this.update();
+    this.save();
   }
 
 }
@@ -34,6 +54,17 @@ export class FavoritesView extends Favorites {
     this.tbody = this.root.querySelector('table tbody');
 
     this.update();
+    this.onadd();
+  }
+
+  onadd() {
+    const addButton = this.root.querySelector('.search button');
+
+    addButton.onclick = () => {
+      const { value } = this.root.querySelector('#input-search');
+      
+      this.add(value);
+    }
   }
 
   update() {
@@ -45,6 +76,7 @@ export class FavoritesView extends Favorites {
 
       row.querySelector('.user img').src = `https://github.com/${user.login}.png`;
       row.querySelector('.user img').alt = `Imagem de ${user.name}`;
+      row.querySelector('.user a').href = `https://github.com/${user.login}`;
       row.querySelector('.user p').textContent = user.name;
       row.querySelector('.user span').textContent = user.login;
       row.querySelector('.repositories').textContent = user.public_repos;
